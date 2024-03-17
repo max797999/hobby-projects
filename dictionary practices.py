e5eb5921c4f2d6cfeeb526472d0398a0d34f2c52
max797999 <163686153+max797999@users.noreply.github.com>
2024-03-17 10:25:26 -0600

    Add files via upload

---
+++ dictionary practices.py
@@ -0,0 +1,88 @@
+governors = {
+    'Alabama': 'Kay Ivey',
+    'Alaska': 'Mike Dunleavy',
+    'Arizona': 'Doug Ducey',
+    'Arkansas': 'Asa Hutchinson',
+    'California': 'Gavin Newsom',
+    'Colorado': 'Jared Polis',
+    'Connecticut': 'Ned Lamont',
+    'Delaware': 'John Carney',
+    'Florida': 'Ron DeSantis',
+    'Georgia': 'Brian Kemp',
+    'Hawaii': 'David Ige',
+    'Idaho': 'Brad Little',
+    'Illinois': 'J.B. Pritzker',
+    'Indiana': 'Eric Holcomb',
+    'Iowa': 'Kim Reynolds',
+    'Kansas': 'Laura Kelly',
+    'Kentucky': 'Andy Beshear',
+    'Louisiana': 'John Bel Edwards',
+    'Maine': 'Janet Mills',
+    'Maryland': 'Larry Hogan',
+    'Massachusetts': 'Charlie Baker',
+    'Michigan': 'Gretchen Whitmer',
+    'Minnesota': 'Tim Walz',
+    'Mississippi': 'Tate Reeves',
+    'Missouri': 'Mike Parson',
+    'Montana': 'Greg Gianforte',
+    'Nebraska': 'Pete Ricketts',
+    'Nevada': 'Steve Sisolak',
+    'New Hampshire': 'Chris Sununu',
+    'New Jersey': 'Phil Murphy',
+    'New Mexico': 'Michelle Lujan Grisham',
+    'New York': 'Kathy Hochul',
+    'North Carolina': 'Roy Cooper',
+    'North Dakota': 'Doug Burgum',
+    'Ohio': 'Mike DeWine',
+    'Oklahoma': 'Kevin Stitt',
+    'Oregon': 'Kate Brown',
+    'Pennsylvania': 'Tom Wolf',
+    'Rhode Island': 'Dan McKee',
+    'South Carolina': 'Henry McMaster',
+    'South Dakota': 'Kristi Noem',
+    'Tennessee': 'Bill Lee',
+    'Texas': 'Greg Abbott',
+    'Utah': 'Spencer Cox',
+    'Vermont': 'Phil Scott',
+    'Virginia': 'Glenn Youngkin',
+    'Washington': 'Jay Inslee',
+    'West Virginia': 'Jim Justice',
+    'Wisconsin': 'Tony Evers',
+    'Wyoming': 'Mark Gordon',
+}
+
+print ('welcome to State Governor look up')
+
+userinput = input('Please enter state abbreviation Your entry must be uppercase')
+if userinput == ("CO"):
+ print (governors["Colorado"])
+
+elif userinput == ("AL"):
+ print (governors["Alabama"])
+
+elif userinput == ("AK"):
+ print (governors["Alaska"])
+
+elif userinput == ("AZ"):
+ print (governors["Arizona"])
+
+elif userinput == ("AK"):
+ print (governors["Arkansas"])
+
+elif userinput == ("CA"):
+ print (governors["California"])
+
+elif userinput == ("CT"):
+ print (governors["Connecticut"])
+
+elif userinput == ("DE"):
+ print (governors["Delaware"])
+
+elif userinput == ("FL"):
+ print (governors["Florida'"])
+
+elif userinput == ("GA"):
+ print (governors["Georgia'"])
+
+elif userinput == ("HI"):
+ print (governors["Hawaii'"])
